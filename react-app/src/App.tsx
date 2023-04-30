071273401e1cb57ad8b883d9d513c89d5eec87d2
Teddy Pascual <teddylayson2000@gmail.com>
2023-05-01 07:30:38 +0800

    Passing function via props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,10 +3,13 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let Places = ["Capas", "Concepcion", "Bamban", "San Vicente", "Cristo Rey"];
   let Friends = ["Nonac", "Andrei", "Anne", "Marie", "Joe","Christopher"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
   return (
     <div>
-      <ListGroup items={Places} heading="Places" />
-      <ListGroup items={Friends} heading="Friends" />
+      <ListGroup items={Places} heading="Places" onSelectItem={handleSelectItem}/>
+      <ListGroup items={Friends} heading="Friends" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
