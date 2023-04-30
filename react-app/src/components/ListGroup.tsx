88b74a2387a6cd7b58f8e27cbe5c3eda126044b3
Teddy Pascual <teddylayson2000@gmail.com>
2023-05-01 07:13:37 +0800

    Finished event Handling and using state hooks

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["Capas", "Concepcion", "Bamban", "San Vicente", "Cristo Rey"];
-  items = [];
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h2>Lezgoooo!</h2>
       <ul className="list-group">
-
-        { items.length === 0 && <p>No item found</p>};
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.length === 0 && <p>No item found</p>};
+        {items.map((item, index) => (
+          <li
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+          >
             {item}
           </li>
         ))}
